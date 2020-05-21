89b6f3f8b06ce03674f3ddbcabfd538f29edd114
Daniel Schimke <danieljschimke@gmail.com>
2020-05-21 18:28:04 -0500

    Created version 1.0 of the homepage

--- server.R
+++ server.R
@@ -1,6 +1,16 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
+  #Updates the screen based on the buttons on the homepage
+  observeEvent(input$changeTask1, {
+    shinydashboard::updateTabItems(session, "explorertabs", "task_1")
+  })
+  observeEvent(input$changeTask2, {
+    shinydashboard::updateTabItems(session, "explorertabs", "task_2")
+  })
+  observeEvent(input$changeDataInfo, {
+    shinydashboard::updateTabItems(session, "explorertabs", "datainfo")
+  })
 })
